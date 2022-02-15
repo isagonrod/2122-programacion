6eff7515c0bc9665284c423d0d808795da5d97cf
isagonzalez <isabelmariagonzalezrodriguez@gmail.com>
2022-02-15 11:57:14 +0100

    Cambios en equals y toString

--- src/Tema7/Act_Aplicacion/Act_15/PP15.java
+++ src/Tema7/Act_Aplicacion/Act_15/PP15.java
@@ -5,14 +5,14 @@ public class PP15 {
         Calendario c1 = new Calendario(2019,1,19);
         Calendario c2 = new Calendario(2022,1,27);
 
-        System.out.println("C1 es igual a C2: " + c1.iguales(c2));
+        System.out.println("C1 es igual a C2: " + c1.equals(c2));
 
         c1.incrementarDia();
         c1.incrementarMes();
         c2.incrementarAño(6);
 
-        c1.mostrar();
-        c2.mostrar();
+        c1.toString();
+        c2.toString();
     }
 
 }
