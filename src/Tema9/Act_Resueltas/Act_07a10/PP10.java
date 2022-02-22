5ef4c2cdf9adf0f85f96d897654f7346cad0d7c7
isagonzalez <isabelmariagonzalezrodriguez@gmail.com>
2022-02-22 11:57:11 +0100

    Modificación en el PP10 (Actividades resueltas del tema 9)

--- src/Tema9/Act_Resueltas/Act_07a10/PP10.java
+++ src/Tema9/Act_Resueltas/Act_07a10/PP10.java
@@ -7,9 +7,9 @@ public class PP10 {
         Socio[] lista = {
                 new Socio(1,"Pepe Pérez","01-01-2021"),
                 new Socio(2,"Marta Martínez","02-02-2002"),
-                new Socio(3,"Juana Putón","02-02-2005"),
-                new Socio(4,"Marica Ramirez","02-02-1998"),
-                new Socio(5,"Bollera Del Campo","02-01-2002")
+                new Socio(3,"Juana Puerta","02-02-2005"),
+                new Socio(4,"María Ramírez","02-02-1998"),
+                new Socio(5,"Carmen del Campo","02-01-2002")
         };
 
         System.out.println(Arrays.toString(lista));
