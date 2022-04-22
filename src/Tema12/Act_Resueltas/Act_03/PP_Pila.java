63afd1d382c1c312ad27b692301a76872bc76595
isagonzalez <isabelmariagonzalezrodriguez@gmail.com>
2022-04-22 10:17:20 +0200

    Actividad resuelta 12.3 (enunciado)

---
+++ src/Tema12/Act_Resueltas/Act_03/PP_Pila.java
@@ -0,0 +1,12 @@
+package Tema12.Act_Resueltas.Act_03;
+
+/* ACTIVIDAD RESUELTA 12.3
+ * Definir la interfaz Pila con parámetros genéricos.
+ * A continuación, implementar la interfaz Pila genérica en la clase Contenedor.
+ * Por último, escribir un programa donde se utilice un objeto contenedor como pila.
+ * En ella apilamos números enteros positivos leídos del teclado hasta que se introduzca un -1.
+ * Después, mediante un bucle, se desapilan todos los números hasta que la pila esté vacía y los mostramos por consola.
+ */
+
+public class PP_Pila {
+}
