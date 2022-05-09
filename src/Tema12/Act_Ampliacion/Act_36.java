b908df9b9765c5eb0f25c1cfec783b506df052d1
Miguel Ángel Ruiz <miguelangel.ruiz@m-bet.com>
2022-05-09 19:03:39 +0200

    Correcciones en ampliación 12.36.

--- src/Tema12/Act_Ampliacion/Act_36.java
+++ src/Tema12/Act_Ampliacion/Act_36.java
@@ -4,6 +4,29 @@ package Tema12.Act_Ampliacion;
  * Repite la actividad de ampliación 9.32 utilizando una colección para guardar y manipular las Llamadas.
  */
 
-public class Act_36 {
+import Tema9.Act_Ampliacion.Act_31a33.ComparaCosteLLamadas;
+import Tema9.Act_Ampliacion.Act_31a33.Llamada;
+import Tema9.Act_Aplicacion.Act_11y12_15a18_24y25.Lista;
+import Utilidades.Fechas;
 
+import java.util.Set;
+import java.util.TreeSet;
+
+public class Act_36 {
+    public static void main (String[] args) {
+        Set<Llamada> conjuntoLlamadas = new TreeSet<>();
+
+        conjuntoLlamadas.add(new Llamada("680701552","676414625",
+                Fechas.formatearFechaCompleta("2022-02-23 19:00"),Fechas.formatearFechaCompleta("2022-02-23 19:08"),
+                true,0));
+        conjuntoLlamadas.add(new Llamada("680701552","676414625",
+                Fechas.formatearFechaCompleta("2022-02-23 19:15"),Fechas.formatearFechaCompleta("2022-02-23 19:20"),
+                false,0));
+
+        Set<Llamada> llamadasOrdenadas = new TreeSet<>(new ComparaCosteLLamadas());
+        llamadasOrdenadas.addAll(conjuntoLlamadas);
+
+        System.out.println(conjuntoLlamadas);
+        System.out.println(llamadasOrdenadas);
+    }
 }
