e861bc393692e102be4d116c71e3eab9b6a9e06b
Miguel Ángel Ruiz <miguelangel.ruiz@m-bet.com>
2022-05-13 17:06:37 +0200

    Correcciones anexo 12.2.

--- src/Tema12/Anexo/Act_2/Matriz2.java
+++ src/Tema12/Anexo/Act_2/Matriz2.java
@@ -1,83 +1,151 @@
 package Tema12.Anexo.Act_2;
 
-import Tema12.Anexo.Act_1.Entero;
-import Tema12.Anexo.Act_1.ListaEntero;
+import Tema12.Anexo.Act_1.*;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.List;
 
 public class Matriz2 {
-    private ArrayList<Entero> matriz;
+    private List<List<Entero>> matriz;
     private int fila;
     private int columna;
 
+    /** Un método constructor que reciba dos enteros indicando el número de filas y columnas e inicialice la matriz con
+     * 	elementos de tipo Entero con valor cero.
+     *
+     * @param nFilas
+     * @param nColumnas
+     */
     public Matriz2(int nFilas, int nColumnas) {
         this.fila = nFilas;
         this.columna = nColumnas;
         this.matriz = new ArrayList<>();
 
-//        for (int i = 0; i < nFilas; i++) {
-//            this.m[i] = new ListaEntero();
-//            for (int j = 0; j < nColumnas; j++) {
-//                this.m[i].getLista().add(new Entero(0));
-//            }
-//        }
         for (int i = 0; i < nFilas; i++) {
+            this.matriz.add(i, new ArrayList<>());
             for (int j = 0; j < nColumnas; j++) {
-                this.matriz.add(new Entero(0)); //TODO: Revisar porque no guarda bien los 0
+                this.matriz.get(i).add(new Entero(0));
             }
         }
     }
 
+    /** Un método constructor que reciba dos enteros con el número de filas y columnas y un array bidimensional de tipo
+     * 	int con los valores iniciales de la matriz.
+     *
+     * @param nFilas
+     * @param nColumnas
+     * @param matriz
+     */
     public Matriz2(int nFilas, int nColumnas, int[][] matriz) {
         this.fila = nFilas;
         this.columna = nColumnas;
-        this.matriz = new ArrayList<>(nFilas);
+        this.matriz = new ArrayList<>();
 
-        // TODO: Hacer el constructor metiéndole la matriz de entrada
+        for (int i = 0; i < nFilas; i++) {
+            this.matriz.add(i, new ArrayList<>());
+            for (int j = 0; j < nColumnas; j++) {
+                this.matriz.get(i).add(new Entero(matriz[i][j]));
+            }
+        }
     }
 
-    public ArrayList<Entero> getMatriz() {
-        return matriz;
+    public List<List<Entero>> getM() {
+        return this.matriz;
     }
 
-    public void setMatriz(ArrayList<Entero> matriz) {
-        this.matriz = matriz;
+    public void setM(List<List<Entero>> m) {
+        this.matriz = m;
     }
 
+    /** Un método numFil que devuelva el número de fila de la matriz.
+     *
+     * @return
+     */
     public int getFila() {
-        return fila;
+        return this.fila;
     }
 
     public void setFila(int fila) {
         this.fila = fila;
     }
 
+    /** Un método numCol que devuelva el número de columna de la matriz.
+     *
+     * @return
+     */
     public int getColumna() {
-        return columna;
+        return this.columna;
     }
 
     public void setColumna(int columna) {
         this.columna = columna;
     }
 
+    /** Un método set que reciba una fila, una columna y un Entero y modifique el valor correspondiente a esa fila y
+     *  columna con el argumento Entero. Utilice para ello el método set de la interfaz List.
+     *
+     * @param fila
+     * @param columna
+     * @param valor
+     */
+    public void set(int fila, int columna, Entero valor) {
+        this.matriz.get(fila).set(columna, valor);
+    }
+
+    /** Un método get que reciba dos argumentos de tipo int y devuelva el Entero que está en la posición indicada.
+     *
+     * @param fila
+     * @param columna
+     * @return
+     */
+    public Entero get(int fila, int columna) {
+        return this.matriz.get(fila).get(columna);
+    }
+
+    /** Un método imprime que muestre la matriz de manera tradicional, es decir, con una fila en cada línea.
+     *  Además, deberá imprimir una primera línea con el mensaje que se le pase como argumento.
+     *
+     * @param cabecera
+     */
     public void imprime(String cabecera) {
         System.out.println(cabecera);
 
-        for(Entero lista : this.matriz) {
+        for(List<Entero> lista : this.matriz) {
             System.out.println(lista);
         }
     }
 
-    //public void set(int fila, int columna, Entero valor) {}
+    /** Un método suma que devuelva en el objeto que invoca la suma de las dos matrices que son pasadas por argumento.
+     * 	Supóngase que todas las matrices tienen el mismo número de filas y columnas.
+     * 	Una vez terminado este método, reescriba el código para que lance dos excepciones distintas para el caso de que
+     * 	las dimensiones de las matrices que se suman no sean iguales y para el caso de que la matriz que invoca la suma
+     * 	no sea de las dimensiones de los sumandos. */
+    public void suma(Matriz2 m1, Matriz2 m2) throws Exception {
+        if (m1.fila != m2.fila) {
+            throw new NumFilaException("Las filas de los sumandos no son iguales");
+        } else if (m1.columna != m2.columna) {
+            throw new NumColException("Las columnas de los sumandos no son iguales");
+        } else if (this.fila != m1.fila) {
+            throw new NumFilaException("Las filas del objeto actual no son iguales a las de los sumandos");
+        } else if (this.columna != m1.columna) {
+            throw new NumColException("Las columnas del objeto actual no son iguales a las de los sumandos");
+        } else {
+            for (int i = 0; i < this.fila; i++) {
+                for (int j = 0; j < this.columna; j++) {
+                    Entero nuevoEntero = new Entero(m1.matriz.get(i).get(j).getNum() + m2.matriz.get(i).get(j).getNum());
+                    this.matriz.get(i).set(j, nuevoEntero);
+                }
+            }
+        }
+    }
 
-    //public Entero get(int fila, int columna) {}
 
-    //public void suma(Matriz m1, Matriz m2) throws Exception {}
-
+    /** Un método denominado espejo de tal manera que el objeto que invoca se modifique, devolviendo una matriz con su
+     * 	imagen especular. */
     public void espejo() {
-        for (Entero e : this.matriz) {
-            Collections.reverse(getMatriz()); //TODO: Revisar
+        for (List<Entero> lista : this.matriz) {
+            Collections.reverse(lista);
         }
     }
 }
--- src/Tema12/Anexo/Act_2/Principal_act2.java
+++ src/Tema12/Anexo/Act_2/Principal_act2.java
@@ -7,9 +7,29 @@ package Tema12.Anexo.Act_2;
  * Implementa un constructor y los métodos set y get para esta implementación de Matriz.
  */
 
+import Tema12.Anexo.Act_1.Entero;
+
 public class Principal_act2 {
-    public static void main(String[] args) {
-        Matriz2 m1 = new Matriz2(2, 3);
-        m1.imprime("\n-- Matriz original --");
+    public static void main(String[] args) throws Exception {
+        Matriz2 matriz1 = new Matriz2(2, 3);
+
+        int[][] tabla = {{1, 2, 3},{4, 5, 6}};
+        Matriz2 matriz2 = new Matriz2(2, 3, tabla);
+
+        matriz1.imprime("\n-- MATRIZ 1 --");
+        matriz2.imprime("\n-- MATRIZ 2 --");
+
+        matriz1.set(0, 0, new Entero(69));
+        matriz1.set(0, 1, new Entero(55));
+        matriz1.set(1, 2, new Entero(88));
+        matriz1.imprime("\n-- MATRIZ 1 (modificada) --");
+
+        System.out.println("\nElemento de la matriz 2 buscado: " + matriz2.get(1, 1));
+
+        matriz1.suma(matriz1, matriz2);
+        matriz1.imprime("\n-- SUMA DE LAS DOS MATRICES --");
+
+        matriz2.espejo();
+        matriz2.imprime("\n-- ESPEJO DE LA MATRIZ 2 --");
     }
 }
